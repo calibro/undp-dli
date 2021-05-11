50b70ac738f5126ac603f5a15a0b22e86853c584
uf0 <scnepa222@gmail.com>
2021-05-11 14:59:15 +0200

    close #1

--- src/lib/StaticViz.svelte
+++ src/lib/StaticViz.svelte
@@ -4,11 +4,14 @@
 
 {#each sections as section}
 	<div class="row my-3 border-bottom border-light">
-		<div class="col-4">
-			<h4>{section.title}</h4>
-			<p>{section.description}</p>
-		</div>
-		<div class="col-8">
+		{#if section.title}
+			<div class="col-4">
+				<h4>{section.title}</h4>
+				<p>{section.description}</p>
+			</div>
+		{/if}
+
+		<div class={`col-${section.title ? '8' : '12'}`}>
 			<div class="d-flex justify-content-center">
 				<img class="img-fluid" src={section.img_path.default} alt={section.title} />
 			</div>
